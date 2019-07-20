89348fdbc0ae458b6ec822674fe0d2fef0c305cc
GlennChon <glenn.chon@gmail.com>
2019-07-20 16:08:31 +0700

    Added className and comment

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+// Return react fragment of gif stills
 const Gif = ({ gif }) => {
   const gifImg = gif.images.fixed_height_still;
   return (
     <React.Fragment>
-      <img src={gifImg.url} height={gifImg.height} width={gifImg.width} />
+      <img
+        className="gif_img"
+        src={gifImg.url}
+        height={gifImg.height}
+        width={gifImg.width}
+      />
     </React.Fragment>
   );
 };
