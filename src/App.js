aef394cb865186a7be808b4ebedcf8b299c35941
GlennChon <glenn.chon@gmail.com>
2019-07-20 16:20:17 +0700

    default search value and updateGifs search added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import _ from "lodash";
+import { getGifs } from "./services/giphyService";
 
 class App extends Component {
-  state = { 
-    gifs:{   }
-   }
-  render() { 
-    return (  );
+  state = {
+    gifs: {},
+    // First real react project, appropriate default search text
+    defaultValue: "I dont know what Im doing"
+  };
+
+  // On first mount, use default search value
+  componentDidMount() {
+    this.updateGifs(this.state.defaultValue);
+  }
+
+  // Get gifs and set state
+  async updateGifs(searchValue) {
+    let json = await getGifs(searchValue);
+    this.setState({ gifs: json });
+    console.log(this.state.gifs);
+  }
+
+  render() {
+    return <div>TEST</div>;
   }
 }
- 
+
 export default App;
