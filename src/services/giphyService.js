3df1ee3b444b3bffe4831e4b39e3bd8390328c1d
GlennChon <glenn.chon@gmail.com>
2019-07-20 14:42:38 +0700

    service for giphy and link creators

---
+++ src/services/giphyService.js
@@ -0,0 +1,21 @@
+import giphy from "./config";
+
+function searchUrl(searchValue) {
+  return `${giphy.apiUrl}/${giphy.query[0]}?api_key=${
+    giphy.apiKey
+  }&q=${searchValue}&limit=${giphy.limit}&offset=${
+    giphy.offset
+  }&rating=G&lang=en`;
+}
+
+function trendingUrl() {
+  return `${giphy.apiUrl}/${giphy.query[1]}?api_key=${giphy.apiKey}&limit=${
+    giphy.limit
+  }&offset=${giphy.offset}&rating=G`;
+}
+
+function randomUrl() {
+  return `${giphy.apiUrl}/${giphy.query[2]}?api_key=${
+    giphy.apiKey
+  }&tag=&rating=G`;
+}
