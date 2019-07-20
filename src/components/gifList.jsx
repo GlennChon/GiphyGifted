33ab1e38977a2d34e5f75a4499dd4084cc4f106b
GlennChon <glenn.chon@gmail.com>
2019-07-20 16:09:17 +0700

    takes gifs and maps to fragment container of divs

--- src/components/gifList.jsx
+++ src/components/gifList.jsx
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
+import React from "react";
+import Gif from "./gif";
 
-class GifList extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
-}
- 
+// Returns react fragment list container of gif divs
+const GifList = ({ gifs }) => {
+  // Map gif objects with key since react requires that
+  const gifList = gifs.map((gif, i) => (
+    <div key={i}>
+      <Gif key={gif.id} gif={gif} />
+    </div>
+  ));
+  return (
+    <React.Fragment>
+      <div className="gif_lst_container">{gifList}</div>
+    </React.Fragment>
+  );
+};
 export default GifList;
