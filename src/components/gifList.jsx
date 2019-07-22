d2f2005f505da54aec8b0c4f5f17559d6d0dd7fa
GlennChon <glenn.chon@gmail.com>
2019-07-22 20:39:27 +0700

    spacing

--- src/components/gifList.jsx
+++ src/components/gifList.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Gif from "./gif";
 import "./gifList.css";
-
 // Returns react fragment list container of gif divs
 const GifList = ({ gifs }) => {
   // Map gif objects with keys
@@ -10,10 +9,12 @@ const GifList = ({ gifs }) => {
       <Gif key={gif.id} gif={gif} />
     </div>
   ));
+
   return (
     <React.Fragment>
       <div className="wrapper">{gifList}</div>
     </React.Fragment>
   );
 };
+
 export default GifList;
