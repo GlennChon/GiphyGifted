8d832e5c8bc4da3afeff62f9dfa62af433125506
GlennChon <glenn.chon@gmail.com>
2019-07-24 12:19:38 +0700

    added jest and empty test files

---
+++ tests/gif.test.js
@@ -0,0 +1,5 @@
+const gif = require("../src/components/gif");
+
+it("<gif> - Expects", () => {
+  expect(true).toBeTruthy();
+});
---
+++ tests/giphyService.test.js
@@ -0,0 +1,50 @@
+import giphyService from "../src/services/giphyService";
+
+// mock function
+// const add = jest.fn(()=>3);
+
+// expect add function to be called once.
+// expect(add).toHaveBeenCalledTimes(1);
+
+// expect add function to be called with 1 and 3 as inputs
+// expect(add).toHaveBeenCalledWith(1,3);
+
+/*
+export async function getGifs(searchValue, offset) {
+    try {
+      let response = await fetch(searchUrl(searchValue, offset));
+      if (!response.ok) {
+        throw response;
+      } else {
+        return response.json();
+      }
+    } catch (err) {
+      alert(err);
+    }
+  }
+  
+  export async function getTrendingGifs(offset) {
+    try {
+      let response = await fetch(trendingUrl(offset));
+      if (!response.ok) {
+        throw response;
+      } else {
+        return response.json();
+      }
+    } catch (err) {
+      alert(err);
+    }
+  }
+  
+  export async function getRandomGif() {
+    try {
+      let response = await fetch(randomUrl());
+      if (!response.ok) {
+        throw response;
+      } else {
+        return response.json();
+      }
+    } catch (err) {
+      alert(err);
+    }
+    */
